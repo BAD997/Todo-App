044610c47766002fb6fce0780185ccb61681d240
Breiner Abello <breinerabello2018@gmail.com>
2025-10-16 15:25:22 -0500

    Implement initial structure for todo store functions and add missing exports

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -22,7 +22,65 @@ const initStore = () => {
 
 }
 
+/**
+ * está función se encarga de guardar el estado en el localStorage
+ */
+const loadStore = () => {
+    throw new Error('Function not implemented.');
+
+}
+
+/**
+ * Esta función se encarga de guardar el estado en el localStorage
+ * @param {String} description recibe la descripción de la nueva tarea pendiente 
+ */
+const addTodo = (description) => {
+    throw new Error('Function not implemented.');
+
+}
+
+/**
+ * Esta función se encarga de cambiar el estado de una tarea pendiente
+ * @param {String} todoId recibe el id de la tarea pendiente a modificar
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('Function not implemented.');
+
+}
+/**
+ * Esta función se encarga de eliminar una tarea pendiente del state
+ * @param {String} todoId  recibe el id de la tarea pendiente a eliminar
+ */
+const deleteTodo = (todoId) => {
+    throw new Error('Function not implemented.');
+
+}
+
+//esta función se encarga de eliminar todas las tareas completadas
+const deleteCompleted = () => {
+    throw new Error('Function not implemented.');
+
+}
+
+/**
+ * esta función se encarga de cambiar el filtro de las tareas pendientes
+ * @param {Array<String>} newFilter recibe el nuevo filtro a aplicar (all, completed, pending)
+ */
+const setFilter = (newFilter = Filter.All) => {
+    throw new Error('Function not implemented.');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Function not implemented.');
+}
+
 export default {
-    
+
+    addTodo,
+    deleteCompleted,
+    getCurrentFilter,
     initStore,
-}   
+    loadStore,
+    setFilter,
+    toggleTodo,
+}
